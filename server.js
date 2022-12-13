7cf172702b703d0decd5c00d31105d9cc8aab610
Abigail Doyle <abigaildoyle3@gmail.com>
2022-12-12 17:03:25 -0800

    server.js handlers and routes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // requirements
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index')
+
+// route files
+const api = require('./routes/api');
+const html = require('./routes/html');
 
 // load express.js
 const app = express();
@@ -9,4 +12,20 @@ const app = express();
 // local host
 const PORT = process.env.PORT || 3001;
 
-//
+// parsing json
+app.use(express.json());
+
+// parsing array
+app.use(express.urlencoded({ extended: true }));
+
+// static files
+app.use(express.static('public'));
+
+// middleware
+app.use('/api', api);
+app.use('/', html);
+
+// local host listener
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
